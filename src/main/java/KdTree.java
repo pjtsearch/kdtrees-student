12d0f6325fb5275fbfc6f8f51b9c5a7418f09f7a
PJTSearch <pjtsignups@gmail.com>
2021-03-12 13:15:51 -0800

    feat(KdTree): Implement size, isEmpty, and insert

--- src/main/java/KdTree.java
+++ src/main/java/KdTree.java
@@ -18,28 +18,113 @@
  *  and nearest-neighbor search (find a closest point to a query point).
  ******************************************************************************/
 
+import java.util.List;
+
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
 
 public class KdTree {
+	
+  private enum NodeVariant {
+	  X,
+	  Y
+  }
+	
+  private class Node<T> {
+	  private NodeVariant variant;
+	  private T data;
+	  private Node<T> lesserChild;
+	  private Node<T> greaterChild;
+
+	  public Node(NodeVariant variant, T data, Node<T> lesserChild, Node<T> greaterChild) {
+		this.variant = variant;
+		this.data = data;
+		this.lesserChild = lesserChild;
+		this.greaterChild = greaterChild;
+	  }
+
+	  public Node(NodeVariant variant, T data) {
+		this.variant = variant;
+		this.data = data;
+	  }
+
+	  public Node(NodeVariant variant) {
+		this.variant = variant;
+	  }
+	  
+	  public String toString(String indent) {
+		  StringBuilder builder = new StringBuilder();
+		  builder.append(indent+data);
+		  if (lesserChild != null || greaterChild != null) {
+			  builder.append(" {\n");
+			  if (lesserChild != null) builder.append(lesserChild.toString(indent+"    "));
+			  if (greaterChild != null) builder.append(greaterChild.toString(indent+"    "));
+			  builder.append(indent+"}");
+		  }
+		  builder.append("\n");
+		  return builder.toString();
+	  }
+  }
+  
+  private Node<Point2D> root;
+  private int size;
 
   // construct an empty set of points
   public KdTree() {
-
+	  root = null;
   }
   // is the set empty? 
   public boolean isEmpty() {
-    return false;
+    return root == null;
   }
+  
+//  private int nodesUnder(Node<Point2D> current) {
+//	  int result = 0;
+//	  if (current.lesserChild != null && current.greaterChild != null) {
+//		  if(current.lesserChild != null) result += nodesUnder(current.lesserChild);
+//		  if(current.greaterChild != null) result += nodesUnder(current.greaterChild);
+//	  } else {
+//		  result = 1;
+//	  }
+//	  return result;
+//  }
   // number of points in the set 
   public int size() {
-    return 0;
+	return size;
+  }
+  
+  private void findAndInsert(Point2D inserted, Node<Point2D> currentParent) {
+	  if (isEmpty()) {
+		  root = new Node<Point2D>(NodeVariant.X, inserted);
+		  return;
+	  }
+	  switch (currentParent.variant) {
+	  	  case X -> {
+	  		  if (inserted.x() < currentParent.data.x()) {
+				  if (currentParent.lesserChild == null) currentParent.lesserChild = new Node<Point2D>(NodeVariant.Y, inserted);
+				  else findAndInsert(inserted, currentParent.lesserChild);
+			  } else {
+				  if (currentParent.greaterChild == null) currentParent.greaterChild = new Node<Point2D>(NodeVariant.Y, inserted);
+				  else findAndInsert(inserted, currentParent.greaterChild);
+			  }
+	  	  }
+	  	  case Y -> {
+	  		  if (inserted.y() < currentParent.data.y()) {
+				  if (currentParent.lesserChild == null) currentParent.lesserChild = new Node<Point2D>(NodeVariant.X, inserted);
+				  else findAndInsert(inserted, currentParent.lesserChild);
+			  } else {
+				  if (currentParent.greaterChild == null) currentParent.greaterChild = new Node<Point2D>(NodeVariant.X, inserted);
+				  else findAndInsert(inserted, currentParent.greaterChild);
+			  }
+	  	  }
+	  }
   }
 
   // add the point to the set (if it is not already in the set)
   public void insert(Point2D p) {
-
+	  findAndInsert(p, root);
+	  size++;
   }
 
   // does the set contain point p? 
@@ -65,6 +150,10 @@ public class KdTree {
   // unit testing of the methods (optional) 
   public static void main(String[] args) {
 
+  }
+  
+  public String toString() {
+	  return root.toString("");
   }
 }
 
