92dc33b0457ed2583210f57f41c0f7d55e00564f
PJTSearch <pjtsignups@gmail.com>
2021-03-23 09:57:07 -0700

    docs(PointSET): Add JavaDoc

--- src/main/java/PointSET.java
+++ src/main/java/PointSET.java
@@ -1,3 +1,4 @@
+
 /******************************************************************************
  *  Name:    J.D. DeVaughn-Brown
  *  NetID:   jddevaug
@@ -6,15 +7,15 @@
  *  Partner Name:    N/A
  *  Partner NetID:   N/A
  *  Partner Precept: N/A
- *  
+ *
  *  Compilation:  javac-algs4 PointSET.java
  *  Execution:    java-algs4 PointSET
- *  Dependencies: Point2D.java RectHV.java 
- * 
- *  Description: Represents a set of points in the unit square 
- *  (all points have x- and y-coordinates between 0 and 1) 
- *  using a red-black BST to support range search 
- *  (find all of the points contained in a query rectangle) 
+ *  Dependencies: Point2D.java RectHV.java
+ *
+ *  Description: Represents a set of points in the unit square
+ *  (all points have x- and y-coordinates between 0 and 1)
+ *  using a red-black BST to support range search
+ *  (find all of the points contained in a query rectangle)
  *  and nearest-neighbor search (find a closest point to a query point).
  ******************************************************************************/
 
@@ -26,66 +27,96 @@ import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.StdDraw;
 
-
+/** A set of points in a red-black BST. */
 public class PointSET {
-	private SET<Point2D> set;
+    /** The set of points. */
+    private SET<Point2D> set;
 
-	// construct an empty set of points
-	public PointSET() {
-		set = new SET<Point2D>();
-	}
-	// is the set empty? 
-	public boolean isEmpty() {
-		return set.isEmpty();
-	}
-	// number of points in the set 
-	public int size() {
-		return set.size();
-	}
+    /** Constructs a new instance of the PointSET class. */
+    public PointSET() {
+        set = new SET<Point2D>();
+    }
 
-	// add the point to the set (if it is not already in the set)
-	public void insert(Point2D p) {
-		if (!set.contains(p)) set.add(p);
-	}
+    /**
+     * Checks whether the set is empty.
+     * @return Whether the set is empty
+     */
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
 
-	// does the set contain point p? 
-	public boolean contains(Point2D p) {
-		return set.contains(p);
-	}
+    /**
+     * Gets the amount of items in the set.
+     * @return The amount of items in the set
+     */
+    public int size() {
+        return set.size();
+    }
 
-	// draw all points to standard draw 
-	public void draw() {
-		for (Point2D point : set) {
-			StdDraw.point(point.x(), point.y());
-		}
-	}
+/**
+     * Adds a point to the set if it is not already in the set.
+     * @param p The point to be inserted.
+     */
+    public void insert(Point2D p) {
+        if (!set.contains(p))
+            set.add(p);
+    }
 
-	// all points that are inside the rectangle
-	public Iterable<Point2D> range(RectHV rect) {
-		if (rect == null) throw new IllegalArgumentException();
-		List<Point2D> result = new ArrayList<>();
-		for (Point2D point : set) {
-			if (rect.contains(point)) result.add(point);
-		}
-		return result;
-	}
+    /**
+     * Checks whether the set contains a point.
+     * @param p The point to be checked for.
+     * @return Whether the set contains a point
+     */
+    public boolean contains(Point2D p) {
+        return set.contains(p);
+    }
 
-	// a nearest neighbor in the set to point p; null if the set is empty 
-	public Point2D nearest(Point2D p) {
-		if (isEmpty()) return null;
-		if (p == null) throw new IllegalArgumentException();
-		List<Point2D> points = new ArrayList<>();
-		for (Point2D point : set) {
-			points.add(point);
-		}
-		points.sort(p.distanceToOrder());
-		return points.get(0);
-	}
+    /** Draws the set with StdDraw. */
+    public void draw() {
+        for (Point2D point : set) {
+            StdDraw.point(point.x(), point.y());
+        }
+    }
 
-	// unit testing of the methods (optional) 
-	public static void main(String[] args) {
+    /**
+     * Finds all points in the set contained in a rectangle.
+     * @param rect The rectangle to be searched.
+     * @return All points in the set contained in a rectangle
+     */
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null)
+            throw new IllegalArgumentException();
+        List<Point2D> result = new ArrayList<>();
+        for (Point2D point : set) {
+            if (rect.contains(point))
+                result.add(point);
+        }
+        return result;
+    }
 
-	}
+    /**
+     * Finds the nearest point in the set to another point.
+     * @param p The point to search for
+     * @return The nearest point in the set to another point
+     */
+    public Point2D nearest(Point2D p) {
+        if (isEmpty())
+            return null;
+        if (p == null)
+            throw new IllegalArgumentException();
+        List<Point2D> points = new ArrayList<>();
+        for (Point2D point : set) {
+            points.add(point);
+        }
+        points.sort(p.distanceToOrder());
+        return points.get(0);
+    }
+
+/**
+     * The CLI entry point.
+     * @param args The CLI args
+     */
+    public static void main(String[] args) {
+
+    }
 }
-
-
