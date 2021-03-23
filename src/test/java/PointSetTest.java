c4c1a8761c54022fc326829db3f82a345883987f
PJTSearch <pjtsignups@gmail.com>
2021-03-23 09:26:22 -0700

    test(PointSetTest): Add more insert, size, and nearest tests

--- src/test/java/PointSetTest.java
+++ src/test/java/PointSetTest.java
@@ -26,41 +26,88 @@ public class PointSetTest {
   @Test
   public void testSize() {
     assertEquals(0, set.size());
-    Point2D newPoint = new Point2D(0, 0);
-    set.insert(newPoint);
-    assertEquals(1, set.size());
-    set.insert(newPoint);
-    assertEquals(1, set.size());
-    set.insert(new Point2D(0, 1));
-    assertEquals(2, set.size());
+    for (int i = 1; i < 100; i++) {
+    	set.insert(new Point2D(i, i));
+        assertEquals(i, set.size());
+    }
   }
 
   @Test
   public void testInsert() {
-    Point2D newPoint = new Point2D(0, 0);
-    set.insert(newPoint);
+    set.insert(new Point2D(0, 0));
     assertEquals(1, set.size());
+    set.insert(new Point2D(1, 0));
+    assertEquals(2, set.size());
+    set.insert(new Point2D(-1, 0));
+    assertEquals(3, set.size());
+    set.insert(new Point2D(-2, -1));
+    assertEquals(4, set.size());
+    set.insert(new Point2D(-2, 2));
+    assertEquals(5, set.size());
+    set.insert(new Point2D(2, -1));
+    assertEquals(6, set.size());
+    set.insert(new Point2D(2, 2));
+    assertEquals(7, set.size());
+    set.insert(new Point2D(2, 2));
+    assertEquals(7, set.size());
+    set.insert(new Point2D(2, -1));
+    assertEquals(7, set.size());
+  }
+  @Test
+  public void testInsertDuplicates() {
+	  for (int i = 1; i <= 500; i++) {
+    	set.insert(new Point2D(i, i));
+    	assertEquals(i, set.size());
+      }
+	  for (int i = 1; i <= 500; i++) {
+    	set.insert(new Point2D(i, i));
+    	assertEquals(500, set.size());
+      }
   }
 
   @Test
   public void testContains() {
-    Point2D newPoint = new Point2D(0, 0);
-    assertFalse(set.contains(newPoint));
-    set.insert(newPoint);
-    assertTrue(set.contains(newPoint));
+    set.insert(new Point2D(0, 0));
+    set.insert(new Point2D(1, 0));
+    set.insert(new Point2D(-1, 0));
+    assertTrue(set.contains(new Point2D(-1, 0)));
+    assertTrue(set.contains(new Point2D(0, 0)));
+    assertTrue(set.contains(new Point2D(1, 0)));
+    assertFalse(set.contains(new Point2D(-2, 0)));
+    for (int i = 1; i < 100; i++) {
+    	set.insert(new Point2D(i, i));
+    }
+    for (int i = 1; i < 100; i++) {
+    	assertTrue(set.contains(new Point2D(i, i)));
+    }
+    for (int i = 100; i < 200; i++) {
+    	assertFalse(set.contains(new Point2D(i, i)));
+    }
   }
 
   @Test
   public void testRange() {
-    RectHV rect = new RectHV(0, 0, 10, 10);
+	RectHV rect = new RectHV(0, 0, 5, 5);
     ArrayList<Point2D> pointsInside = new ArrayList<>();
-    pointsInside.add(new Point2D(0, 0));
-    pointsInside.add(new Point2D(0, 10));
-    pointsInside.add(new Point2D(5, 5));
+    for (int i = 0; i <= 5; i += 1) {
+    	for (int j = 0; j <= 5; j += 1) {
+    		pointsInside.add(new Point2D(i, j));
+        }
+    }
     ArrayList<Point2D> pointsOutside = new ArrayList<>();
-    pointsOutside.add(new Point2D(11, 0));
-    pointsOutside.add(new Point2D(0, 11));
-    pointsOutside.add(new Point2D(11, 11));
+    pointsOutside.add(new Point2D(9, 0));
+    pointsOutside.add(new Point2D(0, 9));
+    pointsOutside.add(new Point2D(8, 8));
+    for (int i = 6; i <= 10; i += 1) {
+    	for (int j = 0; j <= 5; j += 1) {
+    		pointsOutside.add(new Point2D(i, j));
+        }
+    }
+    for (int i = 0; i <= 5; i += 1) {
+    	for (int j = 6; j <= 10; j += 1) {
+    		pointsOutside.add(new Point2D(i, j));
+        }
+    }
     
     for (Point2D p : pointsInside) set.insert(p);
     for (Point2D p : pointsOutside) set.insert(p);
@@ -74,12 +121,13 @@ public class PointSetTest {
 
   @Test
   public void testNearest() {
-	Point2D expected = new Point2D(0, 1);
-    set.insert(expected);
-    set.insert(new Point2D(2, 2));
-    set.insert(new Point2D(0, 5));
+    set.insert(new Point2D(0.7,0.2));
+    set.insert(new Point2D(0.5,0.4));
+    set.insert(new Point2D(0.2,0.3));
+    set.insert(new Point2D(0.4,0.7));
+    set.insert(new Point2D(0.9,0.6));
 
-    assertEquals(expected, set.nearest(new Point2D(0, 0)));
+    assertEquals(new Point2D(0.9, 0.6), set.nearest(new Point2D(0.79, 0.49)));
   }
 
 }
